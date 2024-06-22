372e54abd7e7eb22d4d667d564760a0b98bf5e72
tryPap <t.papagohn@gmail.com>
2024-06-22 20:27:11 +0300

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-function Footer() {
-    return (
-        <footer>
-        <div id="footer">&copy; Data Managment {new Date().toLocaleDateString()}</div>
-      </footer>
-    );
-}
-export default Footer;
